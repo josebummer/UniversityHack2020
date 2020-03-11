e70856493d9aefc11bc7f1ca28b2e8201a526193
josebummer <jantonioruiz@correo.ugr.es>
2020-03-11 13:59:37 +0100

    experimentos

---
+++ src/expermientos1.py
@@ -0,0 +1,231 @@
+import os
+import pickle
+
+import numpy as np
+import pandas as pd
+import progressbar
+from imblearn.pipeline import Pipeline
+from imblearn.under_sampling import EditedNearestNeighbours
+from sklearn.decomposition import PCA
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import f1_score, classification_report
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+from xgboost import XGBClassifier
+
+
+def prepare_data(pdata):
+    data = pdata.copy()
+    data['CADASTRALQUALITYID'] = data['CADASTRALQUALITYID'].map({'9': '0',
+                                                                 '8': '1',
+                                                                 '7': '2',
+                                                                 '6': '3',
+                                                                 '5': '4',
+                                                                 '4': '5',
+                                                                 '3': '6',
+                                                                 '2': '7',
+                                                                 '1': '8',
+                                                                 'C': '9',
+                                                                 'B': '10',
+                                                                 'A': '11', })
+    data['CADASTRALQUALITYID'] = data['CADASTRALQUALITYID'].astype('category')
+
+    return data
+
+
+def fillna(pdata):
+    data = pdata.copy()
+
+    data['MAXBUILDINGFLOOR'].fillna(data['MAXBUILDINGFLOOR'].median(), inplace=True)
+    data['CADASTRALQUALITYID'].fillna(data['CADASTRALQUALITYID'].mode()[0], inplace=True)
+
+    return data
+
+
+def to_numeric(pdata):
+    data = pdata.copy()
+
+    data['CADASTRALQUALITYID'] = data['CADASTRALQUALITYID'].astype(np.int)
+
+    return data
+
+
+def main():
+    # Load and split the data
+    train = pd.read_csv('/home/jose/Escritorio/datathon/src/data/train.txt', sep='|', index_col='ID')
+    labels_ini = train.iloc[:, -1]
+    train.drop('CLASE', axis=1, inplace=True)
+
+    train = prepare_data(train)
+    train = fillna(train)
+    train = to_numeric(train)
+
+    # train, test, yy_train, yy_test = train_test_split(train, labels_ini, test_size=0.2, random_state=42)
+
+    for label in progressbar.progressbar(np.unique(labels_ini)):
+        print('\n--------------------OVA: %s vs All------------------' % label)
+        labels = np.array([1 if x == label else -1 for x in labels_ini])
+
+        X_train, X_test, y_train, y_test = train_test_split(train, labels, test_size=0.2, random_state=42)
+
+        # Construct some pipelines
+        pipe_rf = Pipeline([('scl', StandardScaler()),
+                            ('clf', RandomForestClassifier(random_state=42))])
+
+        pipe_rf_noisy = Pipeline([('scl', StandardScaler()),
+                                  ('enn', EditedNearestNeighbours(random_state=42, sampling_strategy='majority')),
+                                  ('clf', RandomForestClassifier(random_state=42))])
+
+        pipe_rf_pca = Pipeline([('scl', StandardScaler()),
+                                ('pca', PCA(n_components=2, random_state=42)),
+                                ('clf', RandomForestClassifier(random_state=42))])
+
+        pipe_knn = Pipeline([('scl', StandardScaler()),
+                             ('clf', KNeighborsClassifier())])
+
+        pipe_knn_noisy = Pipeline([('scl', StandardScaler()),
+                                   ('enn', EditedNearestNeighbours(random_state=42, sampling_strategy='majority')),
+                                   ('clf', KNeighborsClassifier())])
+
+        pipe_knn_pca = Pipeline([('scl', StandardScaler()),
+                                 ('pca', PCA(n_components=2)),
+                                 ('clf', KNeighborsClassifier())])
+
+        pipe_xgb = Pipeline([('scl', StandardScaler()),
+                             ('clf', XGBClassifier(random_state=42))])
+
+        pipe_xgb_noisy = Pipeline([('scl', StandardScaler()),
+                                   ('enn', EditedNearestNeighbours(random_state=42, sampling_strategy='majority')),
+                                   ('clf', XGBClassifier(random_state=42))])
+
+        pipe_xgb_pca = Pipeline([('scl', StandardScaler()),
+                                 ('pca', PCA(n_components=2)),
+                                 ('clf', XGBClassifier(random_state=42))])
+
+        # Set grid search params
+        grid_params_rf = [{'clf__criterion': ['gini', 'entropy'],
+                           'clf__min_samples_leaf': [1, 3, 5],
+                           'clf__max_depth': [None, 2, 5, 7],
+                           'clf__n_estimators': [50, 100, 400, 900],
+                           'clf__min_samples_split': [2, 5, 8]}]
+
+        grid_params_knn = [{'clf__n_neighbors': [1, 3, 5, 7, 11, 13],
+                            'clf__weights': ['uniform', 'distance'],
+                            'clf__metric': ['minkowski', 'manhattan']}]
+
+        grid_params_xgboost = [{'clf__max_depth': [2, 5, 10, 15],
+                                'clf__learning_rate': [0.01, 0.1, 0.15],
+                                'clf__n_estimators': [100, 500, 1000]}]
+
+        # Construct grid searches
+        jobs = -1
+
+        gs_rf = RandomizedSearchCV(estimator=pipe_rf,
+                                   param_distributions=grid_params_rf,
+                                   scoring='f1',
+                                   cv=5,
+                                   n_jobs=jobs,
+                                   n_iter=20)
+
+        gs_rf_noisy = RandomizedSearchCV(estimator=pipe_rf_noisy,
+                                         param_distributions=grid_params_rf,
+                                         scoring='f1',
+                                         cv=5,
+                                         n_jobs=jobs,
+                                         n_iter=20)
+
+        gs_rf_pca = RandomizedSearchCV(estimator=pipe_rf_pca,
+                                       param_distributions=grid_params_rf,
+                                       scoring='f1',
+                                       cv=5,
+                                       n_jobs=jobs,
+                                       n_iter=20)
+
+        gs_knn = RandomizedSearchCV(estimator=pipe_knn,
+                                    param_distributions=grid_params_knn,
+                                    scoring='f1',
+                                    cv=5,
+                                    n_jobs=jobs,
+                                    n_iter=20)
+
+        gs_knn_noisy = RandomizedSearchCV(estimator=pipe_knn_noisy,
+                                          param_distributions=grid_params_knn,
+                                          scoring='f1',
+                                          cv=5,
+                                          n_jobs=jobs,
+                                          n_iter=20)
+
+        gs_knn_pca = RandomizedSearchCV(estimator=pipe_knn_pca,
+                                        param_distributions=grid_params_knn,
+                                        scoring='f1',
+                                        cv=5,
+                                        n_jobs=jobs,
+                                        n_iter=20)
+
+        gs_xgb = RandomizedSearchCV(estimator=pipe_xgb,
+                                    param_distributions=grid_params_xgboost,
+                                    scoring='f1',
+                                    cv=5,
+                                    n_jobs=jobs,
+                                    n_iter=20)
+
+        gs_xgb_noisy = RandomizedSearchCV(estimator=pipe_xgb_noisy,
+                                          param_distributions=grid_params_xgboost,
+                                          scoring='f1',
+                                          cv=5,
+                                          n_jobs=jobs,
+                                          n_iter=20)
+
+        gs_xgb_pca = RandomizedSearchCV(estimator=pipe_xgb_pca,
+                                        param_distributions=grid_params_xgboost,
+                                        scoring='f1',
+                                        cv=5,
+                                        n_jobs=jobs,
+                                        n_iter=20)
+
+        # List of pipelines for ease of iteration
+        grids = [gs_rf, gs_rf_noisy, gs_rf_pca, gs_knn, gs_knn_noisy, gs_knn_pca, gs_xgb, gs_xgb_noisy, gs_xgb_pca]
+
+        # Dictionary of pipelines and classifier types for ease of reference
+        grid_dict = {0: 'Random Forest', 1: 'Random Forest w/ENN',
+                     2: 'Random Forest w/PCA', 3: 'KNN',
+                     4: 'KNN w/ENN', 5: 'KNN w/PCA',
+                     6: 'XGB', 7: 'XGB w/ENN', 8: 'XGB w/PCA'}
+
+        # Fit the grid search objects
+        print('Performing model optimizations...')
+        best_f1 = 0.0
+        best_clf = 0
+        best_gs = ''
+        for idx, gs in enumerate(grids):
+            print('\nEstimator: %s' % grid_dict[idx])
+            # Fit grid search
+            gs.fit(X_train, y_train)
+            # Best params
+            print('Best params: %s' % gs.best_params_)
+            # Best training data accuracy
+            print('Best training f1: %.3f' % gs.best_score_)
+            # Predict on test data with best params
+            y_pred = gs.predict(X_test)
+            # Test data accuracy of model with best params
+            print('Test set metrics for best params:')
+            print(classification_report(y_test, y_pred))
+            # Track best (highest test f1) model
+            if f1_score(y_test, y_pred) > best_f1:
+                best_f1 = f1_score(y_test, y_pred)
+                best_gs = gs
+                best_clf = idx
+        print('\nClassifier with best test set f1: %s' % grid_dict[best_clf])
+
+        # Save best grid search pipeline to file
+        dump_file = './models/' + label + '_best_gs_pipeline.pkl'
+        with open(dump_file, 'wb') as ofile:
+            pickle.dump(best_gs, ofile)
+        print('\nSaved %s grid search pipeline to file: %s' % (grid_dict[best_clf], dump_file))
+
+
+if __name__ == '__main__':
+    os.environ["PYTHONWARNINGS"] = "ignore::FutureWarning"
+    main()
