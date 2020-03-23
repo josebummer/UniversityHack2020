a9010a822885fa7891329ad0efc4525c80474cb0
Guillermo <guille.eseh@gmail.com>
2020-03-23 22:26:14 +0100

    f1 weights

--- src/aux_scorer.py
+++ src/aux_scorer.py
@@ -2,10 +2,11 @@ from imblearn.pipeline import Pipeline
 from imblearn.base import BaseEstimator
 import numpy as np
 
-from sklearn.metrics import make_scorer, f1_score
-def get_weight_f1(class_weight):
-        def wf1(y,yp):
-            sample_weights = np.array([class_weight[i] for i in y])
-            return f1_score(y,yp,sample_weights)
-        return make_scorer(wf1)
+from sklearn.metrics import f1_score
+class get_weight_f1:
+        def __init__(self,class_weight):
+            self.class_weight = class_weight
 
+        def __call__(self,y,yp):
+            sample_weights = np.array([self.class_weight[i] for i in y])
+            return f1_score(y,yp,sample_weights)
