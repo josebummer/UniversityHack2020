83641e650ce98611e3fa7a1f2d01a45922462bd7
josebummer <jantonioruiz@correo.ugr.es>
2020-04-14 23:22:11 +0200

    probs

--- src/predict_OVA.py
+++ src/predict_OVA.py
@@ -1,12 +1,24 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 import numpy as np
 import os
 import pandas as pd
 from expermientos1 import prepare_data, fillna, to_numeric
 import pickle
 
+
+dump_file = 'data/aggregated_mask_gt0.pkl'
+with open(dump_file, 'rb') as ofile:
+    gt0 = pickle.load(ofile)
+
+gt0['AGRICULTURE'][1] = True
+mask_dict = gt0
+
+
 def main():
-    train = pd.read_csv('/home/jose/Escritorio/datathon/src/data/train.txt', sep='|', index_col='ID')
-    test = pd.read_csv('/home/jose/Escritorio/datathon/src/data/test.txt', sep='|', index_col='ID')
+    train = pd.read_csv('data/train.txt', sep='|', index_col='ID')
+    test = pd.read_csv('data/test.txt', sep='|', index_col='ID')
 
     labels = train.iloc[:, -1]
     train.drop('CLASE', axis=1, inplace=True)
@@ -25,7 +37,7 @@ def main():
     for label in labels_names:
         print('Load %s model:' % label)
 
-        dump_file = './models_nw_dn/' + label + '_best_gs_pipeline.pkl'
+        dump_file = './1models_nw_dn/' + label + '_best_gs_pipeline.pkl'
         with open(dump_file, 'rb') as ofile:
             grid = pickle.load(ofile)
 
@@ -40,19 +52,20 @@ def main():
             y_train = np.array([-1 if x == label else 1 for x in labels])
 
         print('Training...')
-        model.fit(train, y_train)
+        model.fit(train.loc[:, mask_dict[label]], y_train)
 
-        pred_proba = model.predict_proba(train)
+        print('Predicting...')
+        pred_proba = model.predict_proba(test.loc[:, mask_dict[label]])
 
         if label != 'RESIDENTIAL':
             y_pred_label.append(pred_proba[:, 1])
         else:
             y_pred_label.append(pred_proba[:, 0])
 
-    n_data = pd.DataFrame(np.array(y_pred_label).T, index=train.index, columns=labels_names)
+    n_data = pd.DataFrame(np.array(y_pred_label).T, index=test.index, columns=labels_names)
 
-    print('Save new train')
-    n_data.to_csv('data/trainOVA-fit.txt', sep='|')
+    print('Save file')
+    n_data.to_csv('data/test_mask_EVERY_MASK.txt', sep='|')
     # pd.concat([n_data, labels], axis=1).to_csv('data/trainOVA-fit.txt', sep='|')
 
     # y_pred = labels_names[np.argmax(y_pred_label, axis=0)]
